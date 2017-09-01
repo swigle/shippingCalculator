3b16e662d9d528a4f85c656caf730017c449cf49
Jelmer van Dulst <jelmer@swigle.com>
2017-09-01 12:08:17 +0200

    first release version

--- src/ShippingCalculator/ShippingCalculator.php
+++ src/ShippingCalculator/ShippingCalculator.php
@@ -1,17 +1,23 @@
 <?php
-namespace swigle\shippingCalculator;
+namespace Swigle\ShippingCalculator;
 
 class ShippingCalculator {
 	private $shippingSettings;
+	
+	function __construct(ShippingSettings $settings) {
+		$this->setSettings($settings);
+	}
+
 	public function setSettings(ShippingSettings $settings) {
 		$this->shippingSettings = $settings;
 	}
 	
 	public function getShippingCosts($total, $country, $discountRate = 0) {
-		if(!isset($this->shippingSettings[strtolower($country)]) || sizeof($this->shippingSettings[strtolower($country)]) == 0)
+		$shippingSettings = $this->shippingSettings->getDeliveryCostSettings();
+		if(!isset($shippingSettings[strtolower($country)]) || sizeof($shippingSettings[strtolower($country)]) == 0)
 			throw Exception('Country not defined');
 		 
-		$tierPrices = $this->shippingSettings[strtolower($country)];
+		$tierPrices = $shippingSettings[strtolower($country)];
 		$matchingTierPrices = [];
 		
 		foreach(array_keys($tierPrices) as $tierPrice) {
@@ -21,7 +27,6 @@ class ShippingCalculator {
 		
 		$selectedPrice = end($matchingTierPrices);
 		 
-		$discountRate = self::getCustomerDiscountPercentage();
 		if($discountRate > 0)
 			$finalPrice = $selectedPrice - (($selectedPrice / 100) * $discountRate);
 			
--- src/ShippingCalculator/ShippingSettings.php
+++ src/ShippingCalculator/ShippingSettings.php
@@ -1,8 +1,12 @@
 <?php
-namespace swigle\shippingCalculator;
+namespace Swigle\ShippingCalculator;
 
 class ShippingSettings {
 	private $deliveryCosts;
+
+	public function addCountries($costs) {
+		$this->deliveryCosts = $costs;
+	}
 	
 	public function addCountry($code, $costs = [])  {
 		if(isset($deliveryCosts))
@@ -21,7 +25,12 @@ class ShippingSettings {
 		$this->deliveryCosts[$country] = $costs;
 	}
 	
-	public function getDeliveryCostSettings() {
+	public function getDeliveryCostSettings($country = false) {
+		if($country && isset($this->deliveryCosts[$country]))
+			return $this->deliveryCosts[$country];
+		else if($country)
+			throw Exception('No settings known for given country \''.$country.'\'');
+
 		return $this->deliveryCosts;
 	}
 }
